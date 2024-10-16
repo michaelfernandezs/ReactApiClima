b0ab554b8614c1b98ba265d78328be16b81990d4
LENOVO <michaelfernandezdev@gmail.com>
2024-10-16 13:49:44 -0600

    second API CALL

--- src/hooks/UseWeather.ts
+++ src/hooks/UseWeather.ts
@@ -2,11 +2,19 @@ import axios  from "axios"
 import { SearchType } from "../types"
 export  default function  useWeather(){
     const fetchWeather=async(search:SearchType)=>{
-       const appId='a26d897d04526d9b0d8e43b892ef7e72'
+       const appId='25fd904ed3ff9cb1f2b917eb952b1c3d'
        try{
-                const geoURL=`https://api.openweathermap.org/data/2.5/weather?q=${search.city},{search.country}&appid=${appId}`
+                const geoURL=`https://api.openweathermap.org/data/2.5/weather?q=${search.city},
+                ${search.country}&appid=${appId}`
 
-                console.log(geoURL)
+                const {data} = await axios(geoURL)
+                console.log(data)
+                const lat=data[0].lat
+                const lon=data[0].lon
+                const weatherURL=`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid={appId}`
+
+                const {data:weatherResult}= await axios(weatherURL)
+                console.log(weatherResult)
        } catch(error){
 
        }
